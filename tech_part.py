3434a436aed91c9384c96dd046bc7c3fb790b2e5
FeAuCo <vladikyudin09@gmail.com>
2024-09-05 23:00:44 +0300

    Translated into English

--- tech_part.py
+++ tech_part.py
@@ -1,107 +1,103 @@
 import tkinter as tk
 import random
-from visual import window, rus_low, rus_up, eng_low, eng_up, symbols, password_el_dict, lowercase_letters_check, \
-    lowercase_letters, uppercase_letters_check, uppercase_letters, to_include_eng_check, to_include_eng, \
-    to_include_rus_check, to_include_rus, spec_symbols_check, spec_symbols, to_not_include_numbers_check, \
-    to_not_include_numbers, error_label_symbols, amount_of_symbols, gen_btn, to_not_repeat_el_check, \
-    result_entry, cannot_be_generated_label
+import visual
 
 
 # button's funcs
 def act_low():
-    to_include_eng['state'] = tk.NORMAL
-    to_include_rus['state'] = tk.NORMAL
-    if lowercase_letters_check.get() is True:
-        if to_include_eng_check.get() is True:
-            password_el_dict.update({'eng_low_d': eng_low})
-        elif to_include_eng_check.get() is False:
+    visual.to_include_eng['state'] = tk.NORMAL
+    visual.to_include_rus['state'] = tk.NORMAL
+    if visual.lowercase_letters_check.get() is True:
+        if visual.to_include_eng_check.get() is True:
+            visual.password_el_dict.update({'eng_low_d': visual.eng_low})
+        elif visual.to_include_eng_check.get() is False:
             try:
-                password_el_dict.pop('eng_low_d')
+                visual.password_el_dict.pop('eng_low_d')
             except KeyError:
                 pass
-        if to_include_rus_check.get() is True:
-            password_el_dict.update({'rus_low_d': rus_low})
-        elif to_include_rus_check.get() is False:
+        if visual.to_include_rus_check.get() is True:
+            visual.password_el_dict.update({'rus_low_d': visual.rus_low})
+        elif visual.to_include_rus_check.get() is False:
             try:
-                password_el_dict.pop('rus_low_d')
+                visual.password_el_dict.pop('rus_low_d')
             except KeyError:
                 pass
     else:
         try:
-            password_el_dict.pop('rus_low_d')
+            visual.password_el_dict.pop('rus_low_d')
         except KeyError:
             pass
         try:
-            password_el_dict.pop('eng_low_d')
+            visual.password_el_dict.pop('eng_low_d')
         except KeyError:
             pass
-        if uppercase_letters_check.get() is False:
-            to_include_eng['state'] = tk.DISABLED
-            to_include_rus['state'] = tk.DISABLED
+        if visual.uppercase_letters_check.get() is False:
+            visual.to_include_eng['state'] = tk.DISABLED
+            visual.to_include_rus['state'] = tk.DISABLED
 
 
 def act_up():
-    to_include_eng['state'] = tk.NORMAL
-    to_include_rus['state'] = tk.NORMAL
-    if uppercase_letters_check.get() is True:
-        if to_include_eng_check.get() is True:
-            password_el_dict.update({'eng_up_d': eng_up})
-        elif to_include_eng_check.get() is False:
+    visual.to_include_eng['state'] = tk.NORMAL
+    visual.to_include_rus['state'] = tk.NORMAL
+    if visual.uppercase_letters_check.get() is True:
+        if visual.to_include_eng_check.get() is True:
+            visual.password_el_dict.update({'eng_up_d': visual.eng_up})
+        elif visual.to_include_eng_check.get() is False:
             try:
-                password_el_dict.pop('eng_up_d')
+                visual.password_el_dict.pop('eng_up_d')
             except KeyError:
                 pass
-        if to_include_rus_check.get() is True:
-            password_el_dict.update({'rus_up_d': rus_up})
-        elif to_include_rus_check.get() is False:
+        if visual.to_include_rus_check.get() is True:
+            visual.password_el_dict.update({'rus_up_d': visual.rus_up})
+        elif visual.to_include_rus_check.get() is False:
             try:
-                password_el_dict.pop('rus_up_d')
+                visual.password_el_dict.pop('rus_up_d')
             except KeyError:
                 pass
     else:
         try:
-            password_el_dict.pop('rus_up_d')
+            visual.password_el_dict.pop('rus_up_d')
         except KeyError:
             pass
         try:
-            password_el_dict.pop('eng_up_d')
+            visual.password_el_dict.pop('eng_up_d')
         except KeyError:
             pass
-        if lowercase_letters_check.get() is False:
-            to_include_eng['state'] = tk.DISABLED
-            to_include_rus['state'] = tk.DISABLED
+        if visual.lowercase_letters_check.get() is False:
+            visual.to_include_eng['state'] = tk.DISABLED
+            visual.to_include_rus['state'] = tk.DISABLED
 
 
 def no_numbers():
-    if to_not_include_numbers_check.get() is True:
+    if visual.to_not_include_numbers_check.get() is True:
         try:
-            password_el_dict.pop('numbers')
+            visual.password_el_dict.pop('numbers')
         except KeyError:
             pass
     else:
-        password_el_dict.update({'numbers': [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]})
+        visual.password_el_dict.update({'numbers': [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]})
 
 
 def act_symbols():
-    if spec_symbols_check.get() is True:
-        password_el_dict.update({'symbols_d': symbols})
+    if visual.spec_symbols_check.get() is True:
+        visual.password_el_dict.update({'symbols_d': visual.symbols})
     else:
-        password_el_dict.pop('symbols_d')
+        visual.password_el_dict.pop('symbols_d')
 
 
 # checks
 def check_amount_of_symbols():
-    x_amount_of_symbols = amount_of_symbols.get()
+    x_amount_of_symbols = visual.amount_of_symbols.get()
     try:
         x_amount_of_symbols = int(x_amount_of_symbols)
     except ValueError:
-        error_label_symbols.place(relx=.54, rely=.7, anchor="center")
-        amount_of_symbols.delete(0, tk.END)
+        visual.error_label_symbols.place(relx=.54, rely=.7, anchor="center")
+        visual.amount_of_symbols.delete(0, tk.END)
         return False
     else:
         if x_amount_of_symbols <= 4:
-            error_label_symbols.place(relx=.54, rely=.7, anchor="center")
-            amount_of_symbols.delete(0, tk.END)
+            visual.error_label_symbols.place(relx=.54, rely=.7, anchor="center")
+            visual.amount_of_symbols.delete(0, tk.END)
             return False
     return True
 
@@ -110,18 +106,18 @@ def gen_alg():
     global result_list
     result_list = []
     to_not_repeat_el_check_list = []
-    list_with_listed_dict_values = [value_l for value_l in password_el_dict.values()]
-    for i in range(int(amount_of_symbols.get())):
+    list_with_listed_dict_values = [value_l for value_l in visual.password_el_dict.values()]
+    for i in range(int(visual.amount_of_symbols.get())):
         list_to_choose_el_for_password = list_with_listed_dict_values[
             random.randint(0, len(list_with_listed_dict_values) - 1)]
         el_for_password = list_to_choose_el_for_password[random.randint(0, len(list_to_choose_el_for_password) - 1)]
-        if to_not_repeat_el_check.get() is False:
+        if visual.to_not_repeat_el_check.get() is False:
             result_list.append(el_for_password)
             to_not_repeat_el_check_list.append(el_for_password)
-        elif to_not_repeat_el_check.get() is True and el_for_password not in to_not_repeat_el_check_list:
+        elif visual.to_not_repeat_el_check.get() is True and el_for_password not in to_not_repeat_el_check_list:
             result_list.append(el_for_password)
             to_not_repeat_el_check_list.append(el_for_password)
-        elif to_not_repeat_el_check.get() is True and el_for_password in to_not_repeat_el_check_list:
+        elif visual.to_not_repeat_el_check.get() is True and el_for_password in to_not_repeat_el_check_list:
             while el_for_password in to_not_repeat_el_check_list:
                 list_to_choose_el_for_password = list_with_listed_dict_values[
                     random.randint(0, len(list_with_listed_dict_values) - 1)]
@@ -133,69 +129,70 @@ def gen_alg():
 
 # validating
 def too_big_amount_of_symbols():
-    if to_not_repeat_el_check.get():
-        if to_not_include_numbers_check.get():
-            x_amount_of_symbols=0
+    if visual.to_not_repeat_el_check.get():
+        if visual.to_not_include_numbers_check.get():
+            x_amount_of_symbols = 0
         else:
             x_amount_of_symbols = 10
-        if spec_symbols_check.get():
-            x_amount_of_symbols+=7
-        if uppercase_letters_check.get():
-            if to_include_eng_check.get():
+        if visual.spec_symbols_check.get():
+            x_amount_of_symbols += 7
+        if visual.uppercase_letters_check.get():
+            if visual.to_include_eng_check.get():
                 x_amount_of_symbols += 26
-            if to_include_rus_check.get():
+            if visual.to_include_rus_check.get():
                 x_amount_of_symbols += 33
-        if lowercase_letters_check.get():
-            if to_include_eng_check.get():
+        if visual.lowercase_letters_check.get():
+            if visual.to_include_eng_check.get():
                 x_amount_of_symbols += 26
-            if to_include_rus_check.get():
+            if visual.to_include_rus_check.get():
                 x_amount_of_symbols += 33
-        if int(amount_of_symbols.get()) > x_amount_of_symbols:
-            cannot_be_generated_label.place(relx=.54, rely=.7, anchor="center")
+        if int(visual.amount_of_symbols.get()) > x_amount_of_symbols:
+            visual.cannot_be_generated_label.place(relx=.54, rely=.7, anchor="center")
             return False
     return True
 
 
 def cannot_be_generated():
-    if to_not_include_numbers_check.get() is True and spec_symbols_check.get() is False and (
-            (uppercase_letters_check.get() is True or lowercase_letters_check.get() is True) and (
-            to_include_eng_check.get() is False and to_include_rus_check.get() is False) or (
-                    uppercase_letters_check.get() is False and lowercase_letters_check.get() is False)):
-        cannot_be_generated_label.place(relx=.54, rely=.7, anchor="center")
+    if visual.to_not_include_numbers_check.get() is True and visual.spec_symbols_check.get() is False and (
+            (visual.uppercase_letters_check.get() is True or visual.lowercase_letters_check.get() is True) and (
+            visual.to_include_eng_check.get() is False and visual.to_include_rus_check.get() is False) or (
+                    visual.uppercase_letters_check.get() is False and visual.lowercase_letters_check.get() is False)):
+        visual.cannot_be_generated_label.place(relx=.54, rely=.7, anchor="center")
         return False
     return True
 
 
 def gen_end():
-    result_entry.delete(0, 'end')
+    visual.result_entry.delete(0, 'end')
     password = ''
     if cannot_be_generated():
-        if cannot_be_generated_label.winfo_exists():
-            cannot_be_generated_label.place_forget()
+        if visual.cannot_be_generated_label.winfo_exists():
+            visual.cannot_be_generated_label.place_forget()
         if check_amount_of_symbols():
-            if error_label_symbols.winfo_exists():
-                error_label_symbols.place_forget()
+            if visual.error_label_symbols.winfo_exists():
+                visual.error_label_symbols.place_forget()
             if too_big_amount_of_symbols():
-                if cannot_be_generated_label.winfo_exists():
-                    cannot_be_generated_label.place_forget()
-                if to_include_rus_check.get() or spec_symbols_check.get() or uppercase_letters_check.get():
-                    result_entry['width'] = int(amount_of_symbols.get()) + 2
+                if visual.cannot_be_generated_label.winfo_exists():
+                    visual.cannot_be_generated_label.place_forget()
+                if (visual.to_include_rus_check.get() or visual.spec_symbols_check.get() or
+                        visual.uppercase_letters_check.get()):
+                    visual.result_entry['width'] = int(visual.amount_of_symbols.get()) + 2
                 else:
-                    result_entry['width'] = int(amount_of_symbols.get()) + 1
+                    visual.result_entry['width'] = int(visual.amount_of_symbols.get()) + 1
                 gen_alg()
                 for i in result_list:
                     password += str(i)
-            result_entry.insert(0, password)
+            visual.result_entry.insert(0, password)
 
 
 # changing buttons values
 global result_list
-lowercase_letters['command'] = act_low
-uppercase_letters['command'] = act_up
-to_include_rus['command'] = lambda: [act_low(), act_up()]
-to_include_eng['command'] = lambda: [act_low(), act_up()]
-to_not_include_numbers['command'] = no_numbers
-spec_symbols['command'] = act_symbols
-gen_btn['command'] = gen_end
+visual.lowercase_letters['command'] = act_low
+visual.uppercase_letters['command'] = act_up
+visual.to_include_rus['command'] = lambda: [act_low(), act_up()]
+visual.to_include_eng['command'] = lambda: [act_low(), act_up()]
+visual.to_not_include_numbers['command'] = no_numbers
+visual.spec_symbols['command'] = act_symbols
+visual.gen_btn['command'] = gen_end
 # running
-window.mainloop()
+visual.window.mainloop()
--- visual.py
+++ visual.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import Entry
 
 # variables
 window = tk.Tk()
@@ -16,43 +15,43 @@ password_el_dict = {'numbers': [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]}
 ########################################################################################################################
 ########################################################################################################################
 lowercase_letters_check = tk.BooleanVar()
-lowercase_letters = tk.Checkbutton(window, text="Включать строчные буквы", bg="#d2d4d3", activebackground="#d2d4d3",
+lowercase_letters = tk.Checkbutton(window, text="Include lowercase letters", bg="#d2d4d3", activebackground="#d2d4d3",
                                    variable=lowercase_letters_check, onvalue=True, offvalue=False)
 
 uppercase_letters_check = tk.BooleanVar()
-uppercase_letters = tk.Checkbutton(window, text="Включать прописные буквы", bg="#d2d4d3", activebackground="#d2d4d3",
+uppercase_letters = tk.Checkbutton(window, text="Include uppercase letters", bg="#d2d4d3", activebackground="#d2d4d3",
                                    variable=uppercase_letters_check, onvalue=True, offvalue=False)
 
 to_include_eng_check = tk.BooleanVar()
-to_include_eng = tk.Checkbutton(window, text="Включать Латиницу", bg="#d2d4d3", activebackground="#d2d4d3",
+to_include_eng = tk.Checkbutton(window, text="Include Latin", bg="#d2d4d3", activebackground="#d2d4d3",
                                 variable=to_include_eng_check, onvalue=True, offvalue=False, state='disabled')
 
 to_include_rus_check = tk.BooleanVar()
-to_include_rus = tk.Checkbutton(window, text="Включать Кириллицу", bg="#d2d4d3", activebackground="#d2d4d3",
+to_include_rus = tk.Checkbutton(window, text="Include Cyrillic", bg="#d2d4d3", activebackground="#d2d4d3",
                                 variable=to_include_rus_check, onvalue=True, offvalue=False, state='disabled')
 
 spec_symbols_check = tk.BooleanVar()
-spec_symbols = tk.Checkbutton(window, text="Включать спец. символы", bg="#d2d4d3", activebackground="#d2d4d3",
+spec_symbols = tk.Checkbutton(window, text="Include spec. symbols", bg="#d2d4d3", activebackground="#d2d4d3",
                               variable=spec_symbols_check, onvalue=True, offvalue=False)
 
 to_not_include_numbers_check = tk.BooleanVar()
-to_not_include_numbers = tk.Checkbutton(window, text="Не включать цифры", bg="#d2d4d3", activebackground="#d2d4d3",
+to_not_include_numbers = tk.Checkbutton(window, text="Not include numbers", bg="#d2d4d3", activebackground="#d2d4d3",
                                         variable=to_not_include_numbers_check, onvalue=True, offvalue=False)
 
 to_not_repeat_el_check = tk.BooleanVar()
-to_not_repeat_el = tk.Checkbutton(window, text="Не повторять элементы", bg="#d2d4d3", activebackground="#d2d4d3",
+to_not_repeat_el = tk.Checkbutton(window, text="Not repeat elements", bg="#d2d4d3", activebackground="#d2d4d3",
                                   variable=to_not_repeat_el_check, onvalue=True, offvalue=False)
 ########################################################################################################################
 ########################################################################################################################
-settings_label = tk.Label(window, text="Настройки:", bg="#CFCFCF", relief=tk.RAISED)
-amount_of_symbols_label = tk.Label(window, text="Кол-во символов:", bg="#d2d4d3", relief=tk.FLAT)
-result_label = tk.Label(window, text="Результат:", bg="#d2d4d3", relief=tk.GROOVE)
-result_entry: Entry = tk.Entry(window, bg="#d2d4d3", width=20, state='normal')
-cannot_be_generated_label = tk.Label(window, text='Генерация невозможна', bg="#d2d4d3", fg='red')
-error_label_symbols = tk.Label(window, text="Кол-во символов должно быть \nцелым числом, большим 4", bg="#d2d4d3",
+settings_label = tk.Label(window, text="Settings:", bg="#CFCFCF", relief=tk.RAISED)
+amount_of_symbols_label = tk.Label(window, text="Amount of symbols:", bg="#d2d4d3", relief=tk.FLAT)
+result_label = tk.Label(window, text="Result:", bg="#d2d4d3", relief=tk.GROOVE)
+result_entry: tk.Entry = tk.Entry(window, bg="#d2d4d3", width=20, state='normal')
+cannot_be_generated_label = tk.Label(window, text='Generation is impossible', bg="#d2d4d3", fg='red')
+error_label_symbols = tk.Label(window, text="Amount of symbols must be \nInteger which is greater than 4", bg="#d2d4d3",
                                fg="red")
 amount_of_symbols = tk.Entry(window, bg="#d2d4d3", width=4)
-gen_btn = tk.Button(window, text="Сгенерировать", bg="#d2d4d3", activebackground="#d2d4d3")
+gen_btn = tk.Button(window, text="Generate", bg="#d2d4d3", activebackground="#d2d4d3")
 # visual
 window.title("Password Generator")
 window.geometry("300x400+600+100")
@@ -62,16 +61,16 @@ icon = tk.PhotoImage(file='pass_gen_icon.png')
 window.iconphoto(False, icon)
 # running
 settings_label.pack()
-to_include_eng.place(relx=.5, rely=.1, anchor="center")
-to_include_rus.place(relx=.514, rely=.15, anchor="center")
-to_not_include_numbers.place(relx=.502, rely=.2, anchor="center")
-lowercase_letters.place(relx=.563, rely=.25, anchor="center")
-uppercase_letters.place(relx=.575, rely=.3, anchor="center")
-spec_symbols.place(relx=.55, rely=.35, anchor="center")
-to_not_repeat_el.place(relx=.535, rely=.4, anchor="center")
-amount_of_symbols_label.place(relx=.514, rely=.45, anchor="center")
-amount_of_symbols.place(relx=.514, rely=.5, anchor="center")
-gen_btn.place(relx=.514, rely=.60, anchor="center")
-result_label.place(relx=.514, rely=.8, anchor="center")
-result_entry.place(relx=.514, rely=.87, anchor="center")
+to_include_eng.place(x=130, y=40, anchor="center")
+to_include_rus.place(x=135, y=60, anchor="center")
+to_not_include_numbers.place(x=152, y=80, anchor="center")
+lowercase_letters.place(x=160, y=100, anchor="center")
+uppercase_letters.place(x=162, y=120, anchor="center")
+spec_symbols.place(x=155, y=140, anchor="center")
+to_not_repeat_el.place(x=150, y=160, anchor="center")
 
+amount_of_symbols_label.place(x=145, y=195, anchor="center")
+amount_of_symbols.place(x=145, y=215, anchor="center")
+gen_btn.place(x=145, y=250, anchor="center")
+result_label.place(x=145, y=310, anchor="center")
+result_entry.place(x=145, y=335, anchor="center")
